0b59e512979e10e270b7d67b7c4140e2fe9a1063
AnthonyLaw <anthonylaw910@gmail.com>
2024-02-01 10:59:28 -0700

    functions

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,39 +1,32 @@
 export default class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popupElement.classList.add("modal_opened");
     document.addEventListener("keydown", this._handleEscClose);
-    document.addEventListener("mousedown", this._closePopupOutside);
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
     document.removeEventListener("keydown", this._handleEscClose);
-    document.removeEventListener("mousedown", this._closePopupOutside);
   }
 
   _handleEscClose(e) {
-    e.preventDefault();
-
     if (e.key === "Escape") {
       this.close();
     }
   }
 
   setEventListeners() {
-    const closeButtons = document.querySelectorAll(".modal__close");
-    closeButtons.addEventListener("click", () => this.close());
-
-    closeButtons.forEach((button) => {
-      const popup = button.closest(".modal");
-      button.addEventListener("click", () => this.close(popup));
-    });
-    closePopupOutside.addEventListener("click", (e) => {
-      if (e.target === e.currentTarget) {
-        this.close(e.currentTarget);
+    this._popupElement.addEventListener("click", (e) => {
+      if (
+        e.target.classList.contains("modal") ||
+        e.target.classList.contains("modal__close")
+      ) {
+        this.close();
       }
     });
   }
